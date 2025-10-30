cc169926be22ff3344d6938bd20e24d9e01e5239
jponzo <jeremy.ponzo@gmail.com>
2025-10-30 06:43:57 +0100

    better dump cache dbg feature

--- gdextension/src/private/proxy_cache.cpp
+++ gdextension/src/private/proxy_cache.cpp
@@ -84,17 +84,85 @@ bool ORC_ProxyCache::decrement_refcount(int64_t unique_id) {
     return false;
 }
 
+// TODO : put in helper file
+template <typename T>
+static String get_type_name(const Ref<T>& ref) {
+    if (!ref.is_valid()) return "<invalid>";
+    
+    Ref<GDScript> script = ref->get_script();
+    if (script.is_valid() && !script->get_global_name().is_empty()) {
+        return script->get_global_name();
+    } else {
+        return ref->get_class();
+    }
+}
+
+// TODO : put in helper file
+static String get_type_name(const Object* obj) {
+    if (!obj) return "<null>";
+    
+    Ref<GDScript> script = obj->get_script();
+    if (script.is_valid() && !script->get_global_name().is_empty()) {
+        return script->get_global_name();
+    } else {
+        return obj->get_class();
+    }
+}
+
+// TODO : put in helper file
+template <typename T>
+static String get_type_and_address(const Ref<T>& ref) {
+    if (!ref.is_valid()) return "<invalid>";
+    
+    String result = get_type_name(ref);
+    
+    // Add object name if it's a Node
+    Node* node = Object::cast_to<Node>(ref.ptr());
+    if (node) {
+        result += "[\"" + node->get_name() + "\"]";
+    }
+    
+    result += "@" + String::num_int64((int64_t)ref.ptr());
+    return result;
+}
+
+// TODO : put in helper file
+static String get_type_and_address(const Object* obj) {
+    if (!obj) return "<null>";
+    
+    String result = get_type_name(obj);
+    
+    // Add object name if it's a Node
+    const Node* node = Object::cast_to<Node>(obj);
+    if (node) {
+        result += "[\"" + node->get_name() + "\"]";
+    }
+    
+    result += "@" + String::num_int64((int64_t)obj);
+    return result;
+}
+
 String ORC_ProxyCache::dump_cache() const {
     String output = "=== ORC_ProxyCache Dump ===\n";
     
     output += "\n--- Type Cache ---\n";
     output += "Total types: " + String::num_int64(type_cache.size()) + "\n";
     for (const auto& pair : type_cache) {
+        // Determine if this is a PRIMARY or SECONDARY type
+        String category = "";
+        if (!pair.second.empty() && pair.second[0].is_valid()) {
+            if (Object::cast_to<ORC_PrimaryData>(pair.second[0].ptr())) {
+                category = " [--PRIMARY--]";
+            } else if (Object::cast_to<ORC_SecondaryData>(pair.second[0].ptr())) {
+                category = " [--SECONDARY--]";
+            }
+        }
+        
         if (std::holds_alternative<std::type_index>(pair.first.key)) {
-            output += "  [C++] " + String(std::get<std::type_index>(pair.first.key).name()) + 
+            output += "  [C++]" + category + " " + String(std::get<std::type_index>(pair.first.key).name()) + 
                      " -> " + String::num_int64(pair.second.size()) + " instances\n";
         } else {
-            output += "  [GD] " + String(std::get<std::string>(pair.first.key).c_str()) + 
+            output += "  [GD]" + category + " " + String(std::get<std::string>(pair.first.key).c_str()) + 
                      " -> " + String::num_int64(pair.second.size()) + " instances\n";
         }
         
@@ -102,13 +170,7 @@ String ORC_ProxyCache::dump_cache() const {
             const auto& data = pair.second[i];
             output += "    [" + String::num_int64(i) + "] ";
             if (data.is_valid()) {
-                Ref<GDScript> script = data->get_script();
-                if (script.is_valid() && !script->get_global_name().is_empty()) {
-                    output += String(script->get_global_name());
-                } else {
-                    output += String(data->get_class());
-                }
-                output += "@" + String::num_int64((int64_t)data.ptr());
+                output += get_type_and_address(data);
                 
                 ORC_PrimaryData* primary = Object::cast_to<ORC_PrimaryData>(data.ptr());
                 if (primary) {
@@ -116,9 +178,9 @@ String ORC_ProxyCache::dump_cache() const {
                     if (proxy_obj.is_valid()) {
                         Node* node = proxy_obj->get_node();
                         if (node) {
-                            output += "(" + String(node->get_class()) + ":" + node->get_name() + ")";
+                            output += " (" + get_type_and_address(node) + ")";
                         } else {
-                            output += "(node: <null>)";
+                            output += " (node: <null>)";
                         }
                     } else {
                         output += " (proxy_object: <invalid>)";
@@ -128,28 +190,23 @@ String ORC_ProxyCache::dump_cache() const {
                 ORC_SecondaryData* secondary = Object::cast_to<ORC_SecondaryData>(data.ptr());
                 if (secondary) {
                     TypedArray<ORC_PrimaryData> primaries = secondary->get_primary_data_array();
-                    output += " (shared by " + String::num_int64(primaries.size()) + ":";
+                    output += " shared by " + String::num_int64(primaries.size()) + ": \n";
                     for (int j = 0; j < primaries.size(); j++) {
                         Ref<ORC_PrimaryData> prim = primaries[j];
+                        output += "        - ";
                         if (prim.is_valid()) {
-                            Ref<GDScript> prim_script = prim->get_script();
-                            if (prim_script.is_valid() && !prim_script->get_global_name().is_empty()) {
-                                output += " " + String(prim_script->get_global_name());
-                            } else {
-                                output += " " + String(prim->get_class());
-                            }
-                            output += "@" + String::num_int64((int64_t)prim.ptr());
+                            output += get_type_and_address(prim);
                         } else {
-                            output += " <invalid>";
+                            output += "<invalid>";
                         }
-                        if (j < primaries.size() - 1) output += ",";
+                        output += "\n";
                     }
-                    output += ")";
+                } else {
+                    output += "\n";
                 }
             } else {
-                output += "<invalid>";
+                output += "<invalid>\n";
             }
-            output += "\n";
         }
     }
     
@@ -160,13 +217,7 @@ String ORC_ProxyCache::dump_cache() const {
                  " -> refcount: " + String::num_int64(std::get<1>(pair.second));
         const auto& data = std::get<0>(pair.second);
         if (data.is_valid()) {
-            Ref<GDScript> script = data->get_script();
-            if (script.is_valid() && !script->get_global_name().is_empty()) {
-                output += " (" + String(script->get_global_name());
-            } else {
-                output += " (" + String(data->get_class());
-            }
-            output += "@" + String::num_int64((int64_t)data.ptr()) + ")";
+            output += " (" + get_type_and_address(data) + ")";
         } else {
             output += " (<invalid>)";
         }
