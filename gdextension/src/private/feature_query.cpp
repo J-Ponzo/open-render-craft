12f114808f8491bfb2d00ba02a82c0c63357616d
JPonzo <jponzo@wanadev.fr>
2025-11-14 13:45:49 +0100

    simplify feature_query

--- gdextension/src/private/feature_query.cpp
+++ gdextension/src/private/feature_query.cpp
@@ -10,31 +10,8 @@ void ORC_FeatureQuery::_bind_methods() {
     ClassDB::bind_method(D_METHOD("set_value", "value"), &ORC_FeatureQuery::set_value);
     ClassDB::bind_method(D_METHOD("get_value"), &ORC_FeatureQuery::get_value);
     
-    ClassDB::bind_method(D_METHOD("clear"), &ORC_FeatureQuery::clear);
-    
     ADD_PROPERTY(PropertyInfo(Variant::INT, "mask"), "set_mask", "get_mask");
     ADD_PROPERTY(PropertyInfo(Variant::INT, "value"), "set_value", "get_value");
-}
-
-void ORC_FeatureQuery::set_mask(uint64_t p_mask) {
-    mask = p_mask;
-}
-
-uint64_t ORC_FeatureQuery::get_mask() const {
-    return mask;
-}
-
-void ORC_FeatureQuery::set_value(uint64_t p_value) {
-    value = p_value;
-}
-
-uint64_t ORC_FeatureQuery::get_value() const {
-    return value;
-}
-
-void ORC_FeatureQuery::clear() {
-    mask = 0;
-    value = 0;
 }
 
 }
--- gdextension/src/public/feature_query.h
+++ gdextension/src/public/feature_query.h
@@ -15,46 +15,18 @@ namespace godot {
 class ORC_API ORC_FeatureQuery : public RefCounted {
     GDCLASS(ORC_FeatureQuery, RefCounted)
 
-    friend class ORC_ProxyRegistry;
-
-private:
-    uint64_t mask;
-    uint64_t value;
-
 protected:
     static void _bind_methods();
 
 public:
-    ORC_FeatureQuery() : mask(0), value(0) {}
+    uint64_t mask;
+    uint64_t value;
     
-    void set_mask(uint64_t p_mask);
-    uint64_t get_mask() const;
+    void set_mask(uint64_t p_mask) { mask = p_mask; }
+    uint64_t get_mask() const { return mask; }
     
-    void set_value(uint64_t p_value);
-    uint64_t get_value() const;
-    
-    void clear();
-    
-    bool operator==(const ORC_FeatureQuery& other) const {
-        return mask == other.mask && value == other.value;
-    }
-    
-    size_t hash() const {
-        return std::hash<uint64_t>{}(mask) ^ (std::hash<uint64_t>{}(value) << 1);
-    }
-};
-
-struct FeatureQueryHash {
-    size_t operator()(const Ref<ORC_FeatureQuery>& query) const {
-        return query.is_valid() ? query->hash() : 0;
-    }
-};
-
-struct FeatureQueryEqual {
-    bool operator()(const Ref<ORC_FeatureQuery>& a, const Ref<ORC_FeatureQuery>& b) const {
-        if (!a.is_valid() || !b.is_valid()) return a.ptr() == b.ptr();
-        return *a == *b;
-    }
+    void set_value(uint64_t p_value) { value = p_value; }
+    uint64_t get_value() const { return value; }
 };
 
 }
