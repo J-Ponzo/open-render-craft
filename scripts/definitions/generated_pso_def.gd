0ed4cd01b1970034a8fda689323440c372ce2a5a
JPonzo <jponzo@wanadev.fr>
2025-12-05 12:44:43 +0100

    add generated pso def

---
+++ scripts/definitions/generated_pso_def.gd
@@ -0,0 +1,4 @@
+extends ORC_PSODef
+class_name ORC_GeneratedPSODef
+
+@export var factory_impl : ORC_AImpl_Def
--- scripts/definitions/render_pass_def.gd
+++ scripts/definitions/render_pass_def.gd
@@ -5,3 +5,4 @@ class_name ORC_RenderPassDef
 @export var pass_impl : ORC_AImpl_Def
 @export var fb_format_def : ORC_FramebufferFormat_Def
 @export var explicite_pso_defs : Dictionary[StringName, ORC_ExpicitPSODef]
+@export var generated_pso_defs : Dictionary[StringName, ORC_GeneratedPSODef]
